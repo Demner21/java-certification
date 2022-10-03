5bda4040fb1d37c78eefd987d835bbd4bb840060
Renoir <demner21@gmail.com>
2022-10-02 20:42:07 -0500

    practice operators chapter

--- book1/2-operators/Operators.java
+++ book1/2-operators/Operators.java
@@ -16,6 +16,24 @@ public class Operators{
         System.out.println(b.equals(d))    ;
         System.out.println(b.trim().equals(d.trim()))    ;
 
+        System.out.println("----");
 
+        //Test post unary operator
+        var test= 5;
+        //var test += --test + test++; not compile
+        //my answer: 14 - true answer: not compile
+
+        //test = --test + test++;
+        //my answer: 9 - true answer: 8
+
+        var demo= 9.8;
+        //my answer:9  - true answer:9
+        System.out.println((int) demo);
+        
+        test= test++ *  --test ;
+        
+        //my answer:25  - true answer: 25
+
+        System.out.println(test);
     }
 }
---
+++ theory/2_operators/weird_examples.md
@@ -0,0 +1,12 @@
+> this operation is not valid
+
+> This not execute! :x:
+```
+$ java --classpath ./classes/ Operators
+
+```
+> Bad use of assigment += :x:
+```
+ var test= 5;
+ var test += --test + test++;
+```
