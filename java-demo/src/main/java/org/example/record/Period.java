a9570cabb1ca2e75f32faaed0b596add1939a0b0
Raul Demner <rauldemner.lopez@globant.com>
2023-03-28 17:26:41 -0500

    add examples for chapter 7

--- java-demo/src/main/java/org/example/record/Period.java
+++ java-demo/src/main/java/org/example/record/Period.java
@@ -6,4 +6,18 @@ public record Period(int minutes) {
         this(0);
     }
 
+    public Period(int m, int n) {
+        this(getTmp(m, n));
+    }
+
+    private static int getTmp(int m, int n) {
+        var tmp = 0;
+        if (m > n) {
+            tmp = m - n;
+        } else {
+            tmp = n - m;
+        }
+        return tmp;
+    }
+
 }
